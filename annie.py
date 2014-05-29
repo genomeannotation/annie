ca8bcbf5b0e53d1dff09270d83b7df52d49c8559
ben-guin <reubent@hawaii.edu>
2014-05-29 12:50:22 -1000

    Conformed PEP 8 standards

--- annie.py
+++ annie.py
@@ -3,26 +3,26 @@ import sys
 
 #The command-line arguments will be:
 #Case 1: IPRScan
-    # ipr <iprFileName> <outputFileName>
+    # ipr <ipr_file_name> <output_file_name>
 #Case 2: Sprot Scan
-    # sprot <blastoutFileName> <gffFileName> <fastaFileName> <outputFileName>
+    # sprot <blastout_file_name> <gff_file_name> <fasta_file_name> <output_file_name>
 
 def main(args):
     case = args[1]
     if case == "ipr":
-        iprFile = open(args[2], 'r')
-        fileOutput = open(args[3], 'w')
-        results = read_ipr(iprFile)
+        ipr_file = open(args[2], 'r')
+        file_output = open(args[3], 'w')
+        results = read_ipr(ipr_file)
     elif case == "sprot":
-        blastFile = open(args[2], 'r')
-        gffFile = open(args[3], 'r')
-	fastaFile = open(args[4], 'r')
-	fileOutput = open(args[5], 'w')
-	results = read_sprot(blastFile, gffFile, fastaFile)
+        blast_file = open(args[2], 'r')
+        gff_file = open(args[3], 'r')
+	fasta_file = open(args[4], 'r')
+	file_output = open(args[5], 'w')
+	results = read_sprot(blast_file, gff_file, fasta_file)
     else:
 	print "That case is not yet supported"
 	exit()
-    writeResults(results, fileOutput)
+    write_results(results, file_output)
 
 def read_ipr(io_buffer):
     """Returns a list of lists, each containing mrna_id, "Dbxref" and annotation."""
@@ -36,22 +36,22 @@ def read_ipr(io_buffer):
                              == 0 or ipr_list[i] != ipr_list[i-1]]
     return ipr_list
 
-def read_sprot(blastFile, gffFile, fastaFile):
-    fastaInfo = getFastaInfo(fastaFile)
-    gffInfo = getGffInfo(gffFile)
-    blastInfo = getBlastInfo(blastFile)
+def read_sprot(blast_file, gff_file, fasta_file):
+    fasta_info = get_fasta_info(fasta_file)
+    gff_info = get_gff_info(gff_file)
+    blast_info = get_blast_info(blast_file)
     sprot_list = []
-    for mRNA in gffInfo:
-	product = fastaInfo[blastInfo[mRNA]][0]
-	geneName = fastaInfo[blastInfo[mRNA]][1]
-	geneID = gffInfo[mRNA]
-	sprot_list.append([geneID, "name", geneName])
-	sprot_list.append([mRNA, "product", product])
+    for mrna in gff_info:
+	product = fasta_info[blast_info[mrna]][0]
+	gene_name = fasta_info[blast_info[mrna]][1]
+	gene_id = gff_info[mrna]
+	sprot_list.append([gene_id, "name", gene_name])
+	sprot_list.append([mrna, "product", product])
     return sprot_list
 
-def getFastaInfo(fastaFile):
-    myDict = {}
-    for line in fastaFile:
+def get_fasta_info(fasta_file):
+    dbxrefs = {}
+    for line in fasta_file:
 	if line[0] == '>':
 	    words = line.split(" ")
             ref = words[0][1:]
@@ -69,31 +69,31 @@ def getFastaInfo(fastaFile):
 		name = (" ".join(words[i:j]))[3:]		
 	    else:
 		name = "UNNAMED"
-	    myDict[ref] = (product,name)
-    return myDict
+	    dbxrefs[ref] = (product,name)
+    return dbxrefs
 
-def getBlastInfo(blastFile):
-    myDict = {}
-    for line in blastFile:
+def get_blast_info(blast_file):
+    mrna_dbxrefs = {}
+    for line in blast_file:
         columns = line.split("\t")
         mrna = columns[0]
 	ref = columns[1]
-	myDict[mrna] = ref
-    return myDict
+	mrna_dbxrefs[mrna] = ref
+    return mrna_dbxrefs
 
-def getGffInfo(gffFile):
-    myDict = {}
-    for line in gffFile:
+def get_gff_info(gff_file):
+    mrna_genes = {}
+    for line in gff_file:
 	columns = line.split("\t")
 	if len(columns)>1 and columns[2] == "mRNA":
-	    mRNA_ID = (columns[8].split(";")[0])[3:]
-	    parentGene = (columns[8].split(";")[1])[7:-1]
-	    myDict[mRNA_ID] = parentGene
-    return myDict
+	    mrna_id = (columns[8].split(";")[0])[3:]
+	    parent_gene = (columns[8].split(";")[1])[7:-1]
+	    mrna_genes[mrna_id] = parent_gene
+    return mrna_genes
 
 
-def writeResults(results, fileOut):
+def write_results(results, file_out):
     for line in results:
-        fileOut.write("\t".join(line)+"\n")
+        file_out.write("\t".join(line)+"\n")
 
 main(sys.argv)
